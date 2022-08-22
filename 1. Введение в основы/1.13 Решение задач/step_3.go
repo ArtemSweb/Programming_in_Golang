4752191c46aa996491b8a48ec5a5932660d127f5
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-08-22 17:56:05 +0300

    Вариант решения 1.13.3

---
+++ 1. Введение в основы/1.13 Решение задач/step_3.go
@@ -0,0 +1,13 @@
+// Дано трехзначное число. Переверните его, а затем выведите
+
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Scan(&n)
+
+	fmt.Printf("%d%d%d", (n % 10), (n / 10 % 10), (n / 100 % 10))
+}
