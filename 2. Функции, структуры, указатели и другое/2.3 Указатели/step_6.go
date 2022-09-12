07e85fa8b763a8a3223876bf5ef01cb35d1d2d4c
ArtemSweb <solodovnikov-artem@inbox.ru>
2022-09-12 14:29:32 +0300

    вариант решения 2.3.6

--- 2. Функции, структуры, указатели и другое/2.3 Указатели/step_6.go
+++ 2. Функции, структуры, указатели и другое/2.3 Указатели/step_6.go
@@ -8,10 +8,11 @@ package main
 import "fmt"
 
 func main() {
-	a, b := 2, 2
+	a, b := 2, 5
 	test(&a, &b)
 }
 
-func(test)(x1 *int, x2 *int){
-	fmt.Println(*x1 * *x2)
+func test(x1, x2 *int) {
+	*x1, *x2 = *x2, *x1
+	fmt.Println(*x1, *x2)
 }
